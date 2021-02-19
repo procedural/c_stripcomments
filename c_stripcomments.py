6723ac488e3295208702b41757fe68d3e4690738
procedural <iamvfx@gmail.com>
2021-02-19 14:12:28 +0300

    Update c_stripcomments.py

--- c_stripcomments.py
+++ c_stripcomments.py
@@ -9,7 +9,7 @@ try:
     print("c_stripcomments.py error: pass the path to a file as the second argument.")
   else:
     comments = pyparsing.nestedExpr("/*", "*/").suppress()
-    with io.open(sys.argv[1], 'r', encoding='utf8') as fd:
+    with io.open(sys.argv[1], "r", encoding="utf8") as fd:
       print(comments.transformString(fd.read()))
 except:
   print("c_stripcomments.py error:", sys.exc_info())
